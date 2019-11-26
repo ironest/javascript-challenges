de5bf8befcef2bb76efb86c6687af7de8b1a4ee2
ironest <ironest@gmail.com>
2019-11-26 19:28:17 +1100

    Added Ch3-Ex01

---
+++ challenge_03/exercise_01.js
@@ -0,0 +1,11 @@
+const capitaliseTheFront = (string) => {
+    
+	let words = string.split(" ");
+	
+	for(idx in words){
+		word = words[idx].toLowerCase();
+		words[idx] = word.charAt(0).toUpperCase() + word.slice(1);
+	}
+	
+	return words.join(" ");
+}
