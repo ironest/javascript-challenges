ab0c079ddadd074c6db4dbdd8f6829f44ace8150
ironest <ironest@gmail.com>
2019-11-29 09:33:00 +1100

    Added vanilla and ES6 version for Ch07-Ex01

--- challenge_07/exercise_01.js
+++ challenge_07/exercise_01.js
@@ -1,5 +1,18 @@
-const multipleArguments = ( ...params ) => {
+function multipleArguments(){
+	
+	let result = 1;
+	if (arguments.length === 0) {
+		return 0;
+	}
+	
+	for(let num of arguments){
+		result *= num;
+	}
+	return result;
+}
 
+const multipleArguments_ES6 = ( ...params ) => {
+	
 	let result = 1;
 	if (params.length === 0) {
 		return 0;
@@ -10,3 +23,6 @@ const multipleArguments = ( ...params ) => {
 	}
 	return result;
 }
+
+console.log(multipleArguments(1, 2, 3,4, 5, 6));
+console.log(multipleArguments_ES6(1, 2, 3,4, 5, 6));
