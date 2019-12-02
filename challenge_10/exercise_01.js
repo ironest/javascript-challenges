44bed29b7cbdd0ffb00360e43f89f6d479216df9
ironest <ironest@gmail.com>
2019-12-03 01:57:18 +1100

    Fixed reduce exercise in Ch10

--- challenge_10/exercise_01.js
+++ challenge_10/exercise_01.js
@@ -126,9 +126,13 @@ console.log(totalYear);
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
 
 console.log("Exercise 13");
-let superString = data.reduce((acc, curr) => {
-  return acc + curr  
-})
-console.log(superString);
+
+let instanceData = data.reduce((acc, curr) => {
+  if (!acc[curr]) acc[curr] = 0;
+  acc[curr]++; 
+  return acc;
+}, {});
+
+console.log(instanceData);
 
 // source: wes bos (edited by harrison and aaron)
