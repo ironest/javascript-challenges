d1a0ab178f38378697b2217c2d757fc05df8303e
ironest <ironest@gmail.com>
2019-12-18 00:01:20 +1100

    Added Footer React component

---
+++ challenge_23/react_challenge/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <h5>Thanks for visiting my profile. Hope to here from you soon!</h5>
+        </footer>
+    )
+
+}
+
+export default Footer
