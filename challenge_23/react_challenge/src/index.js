2c26e9c0294666fd305b6d01d9a334c762218290
ironest <ironest@gmail.com>
2019-12-18 23:59:05 +1100

    Added React entry point

---
+++ challenge_23/react_challenge/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+    // React.createElement("h1", null, "Does this work?"),
+    <App />,
+    document.getElementById("root")
+);
